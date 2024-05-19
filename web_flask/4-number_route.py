c625bfd8627daaf8507ec16d3ee707b2a1f99b9a
yasabri <yasabri04@gmail.com>
2024-05-19 10:41:44 -0700

    me

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,10 +1,9 @@
-
 #!/usr/bin/python3
 """Starts a Flask web application.
-
-application listens on 0.0.0.0, port 5000
+ application listens on 0.0.0.0, port 5000
 """
 from flask import Flask
+from flask import abort
 
 app = Flask(__name__)
 
@@ -21,6 +20,9 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """Displays 'C' followed by the value of <text>.
+
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
@@ -28,8 +30,16 @@ def c(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+
+    """
     text = text.replace("_", " ")
     return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
