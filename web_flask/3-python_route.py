594edd53d0ca50bc8cb5c26d29dc5e196a72519a
yasabri <yasabri04@gmail.com>
2024-05-19 10:31:34 -0700

    me

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+application listens on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
